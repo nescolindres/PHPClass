b4253297cadab17493696253074e2a6a4f8f9a49
nescolindres <44357907+nescolindres@users.noreply.github.com>
2021-03-01 15:37:37 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,120 @@
+<?php
+
+$company_name = "Colindres-Benz Automaker";
+$company_address = "1234 Automaker Dr.";
+$company_citystatezip = "Los Angeles, CA. 91344";
+$debug;
+$car_array;
+
+print "<h2>Lab 5 Assignment</h2>";
+
+function  create_array_cars (  )  {
+    global $car_array;
+    $car_array = array();
+    $car_array[] = "ID: 12345, Vehicle: 2002 Ford Ranger, $6500.00, Excellent condition, low 68000 miles" ;
+    $car_array[] = "ID: 45678, Vehicle: 1998 Chevy Corvette, $19995.00, Low miles 54000, Great car 4 cruising" ;
+    $car_array[] = "ID: 67890, Vehicle: 2000 Toyota Camry, $9990.00, Mom wants you 2 buy a conservative car" ;
+    $car_array[] = "ID: 89123, Vehicle: 1995 Honda Civic, $4500.00, 140000 miles, but a Honda, it will last" ;
+}
+
+create_array_cars();
+
+function displayProduct($localvar) {
+	
+	print "<hr> Cars for sale! </hr><br>";
+	
+print "$localvar[0]<br>";
+print "$localvar[1]<br>";
+print "$localvar[2]<br>";
+print "$localvar[3]<br>";
+	
+}
+
+displayProduct($car_array);
+
+
+print "<hr>";   //    This draws a line between lab assignments
+
+
+
+print "<h2>Lab 4 Assignment</h2>";
+
+
+print display_company_name($company_name);
+print display_company_address();
+
+
+
+
+print "<hr>";   //    This draws a line between lab assignments
+
+
+function display_company_name ($localvar) {
+	
+	print "<h> My company is $localvar </h>";
+	$localvar = "Colindres-Benz Automaker";
+	
+}
+
+function display_company_address() {
+	global $company_name;
+	global $company_address;
+	global $company_citystatezip;
+	
+	print "<b><i> $company_name $company_address $company_citystatezip </b></i>";
+}
+
+
+
+// ----------------------------------   lab 4   --------------------------------
+if (!empty($_REQUEST['debug'])) {
+   $debug = true;
+   print "DEBUG turned ON<br>";
+}
+else {
+   $debug = false;
+}
+
+
+print "<h2>Lab 3 Assignment</h2>";
+
+print " <a href='http://nestorcolindres.infinityfreeapp.com?debug=true'>Debug ON</a> ";
+print " <a href='http://nestorcolindres.infinityfreeapp.com'>Debug OFF</a> ";
+
+
+
+
+if ($debug == true) { print "Now executing Lab 3, \$company_name contains $company_name<br>"; }
+
+else {
+	
+}
+
+print "<hr>";
+
+
+print "<h2>Lab 2 Assignment</h2>";
+
+
+if ($debug == true ) {
+   print "Now executing Lab 3, \$company_name contains $company_name<br>";
+}
+
+print "<h1> $company_name</h1>";
+
+print "$company_address $company_citystatezip";
+    
+
+print "<hr>";   //    This draws a line between lab assignments
+
+//---------------------     A comment line to separate each lab    ---------------------------
+
+print "<H2>Lab 1 Assignment</H2>";
+
+   print "My name is Nestor Colindres";
+   
+   if ($debug == true ) {
+   print "Now executing Lab 3, \$company_name contains $company_name<br>";
+}
+
+?>
